694ba4530bc0f915290ecc11b100e53b8e045bef
Milo Lee <leexu.job@gmail.com>
2019-02-26 21:16:03 +0800

    Bump version to 0.0.5

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Bump.Mixfile do
   def project do
     [
       app: :mix_bump,
-      version: "0.0.4",
+      version: "0.0.5",
       elixir: "~> 1.5",
       elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
